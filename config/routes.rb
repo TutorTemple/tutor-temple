a90a2142edd74d133f117fe7bc0fc88498f0ee11
Smithe941 <onupriy94@gmail.com>
2018-07-30 18:10:41 +0300

    Update SignUp page with switch role

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-
-  devise_for :users
-
-  resources :dashboard
-
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   get ':page_name', controller: :pages, action: :index, constraints: { page_name: /(about|contacts|become_a_tutor|help)/ }
+  resources :dashboard
+  root 'pages#index'
 end
