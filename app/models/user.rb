916b09a54ed4820d456cc1386b8d982070efd802
Smithe941 <onupriy94@gmail.com>
2018-07-30 13:52:14 +0300

    Add roles to user with enumerize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  extend Enumerize
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enumerize :role, in: [:student, :tutor, :admin]
 end
