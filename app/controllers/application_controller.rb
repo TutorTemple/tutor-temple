29e7c770707ee3c2b18c385d42ff34db2eac16aa
Smithe941 <onupriy94@gmail.com>
2018-07-31 17:47:12 +0300

    Add spec to registration controller [WIP]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource)
     dashboard_index_path
