a05949c31d8a6e1e15cad0427bdf36b93a3d9648
Smithe941 <onupriy94@gmail.com>
2018-07-31 19:01:10 +0300

    Add spec to registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,11 +10,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:terms, :role])
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
-  end
-
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
---
+++ spec/features/devise/registrations_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Registrations", type: :feature do
+  context "configure_sign_up_params" do
+    let(:user) { build(:student) }
+    scenario "should be successful" do
+      visit new_user_registration_path
+      within('form') do
+        fill_in 'Email', with: user.email
+        fill_in 'user_password', with: user.password
+        fill_in 'user_password_confirmation', with: user.password
+        choose 'user_role_student'
+        check 'user_terms'
+        click_on 'Sign Up'
+      end
+      expect(page).to have_content('Dashboard')
+    end
+
+    scenario "should be failure" do
+      visit new_user_registration_path
+      within('form') do
+        fill_in 'Email', with: user.email
+        fill_in 'user_password', with: user.password
+        fill_in 'user_password_confirmation', with: user.password
+        choose 'user_role_student'
+        click_on 'Sign Up'
+      end
+      expect(page).to have_content('Terms must be accepted')
+    end
+  end
+end
