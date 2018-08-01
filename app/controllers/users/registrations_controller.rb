524897c07385f82858842303f8866a8425259b44
DeeMak13 <dima.m@active-bridge.com>
2018-08-01 12:16:21 +0300

    Fix rubocop warnings

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:terms, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[terms role])
   end
 
   # The path used after sign up.
