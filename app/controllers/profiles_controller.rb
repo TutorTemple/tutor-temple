4fe8919b3d643b53298e1fe29c013b7d4ff52d30
Volodymyr Kovtun <volodymyr.k@active-bridge.com>
2018-08-01 13:55:05 +0300

    Add profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,22 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   expose :profile, (-> { current_user.profile })
+
+  def create
+    current_user.profile.create(profile_params)
+  end
+
+  def update
+    if profile.update(profile_params)
+      flash[:notice] = 'Your profile successfully updated'
+    else
+      render 'profiles/edit'
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :gender, :birthday, :phone_number, :about_me)
+  end
 end
