cabcb3038a9b7466ecb292c271b6deee1a477d22
Smithe941 <onupriy94@gmail.com>
2018-08-03 12:24:33 +0300

    Refactor spec for subject

--- spec/features/admin/subject_spec.rb
+++ spec/features/admin/subject_spec.rb
@@ -24,6 +24,20 @@ RSpec.feature 'New subject', type: :feature do
         fill_in 'Name', with: 'Math'
       end
       click_on 'Create Subject'
+      expect(page).to have_content('Subject was successfully created.')
+    end
+
+    scenario 'should be failure' do
+      create(:subject, name: 'hui')
+      click_link 'Subjects'
+      expect(page).to have_content('New Subject')
+      click_link 'New Subject'
+      expect(page).to have_content('New Subject')
+      within('form') do
+        fill_in 'Name', with: 'hui'
+      end
+      click_on 'Create Subject'
+      expect(page).to have_content('has already been taken')
     end
   end
 end
