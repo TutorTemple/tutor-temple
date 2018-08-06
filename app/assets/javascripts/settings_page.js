d7997db53f4f7d5ca1271faadc628b29252c9551
Smithe941 <onupriy94@gmail.com>
2018-08-06 16:53:05 +0300

    Add tabs on settings page

---
+++ app/assets/javascripts/settings_page.js
@@ -0,0 +1,21 @@
+$(document).on('turbolinks:load', function() {
+  document.getElementById("defaultOpen").click();
+});
+
+function changeTabs(evt, tab) {
+
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
