dad8e221e3e9dd6aa0210f8c8abe1349b73e3b67
DeeMak13 <dima.m@active-bridge.com>
2018-08-06 09:58:08 +0300

    Permit languages and timezone, add store accessors and validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,16 @@
 class Profile < ApplicationRecord
+  enum gender: { male: 0, female: 1 }.freeze
+
   mount_uploader :avatar, AvatarUploader
 
   belongs_to :user
   has_many :profile_subjects
   has_many :subjects, through: :profile_subjects
 
-  enum gender: { male: 0, female: 1 }.freeze
+  store_accessor :meta, :languages
+  store_accessor :meta, :time_zone
 
-  validates :first_name, :last_name, :gender, :birthday, presence: true
+  validates :first_name, :last_name, :gender, :birthday, :time_zone, :languages, presence: true
   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: I18n.t('profiles.error.names') },
                                      length: { in: 2..15 }
   validate :not_future_birthday
