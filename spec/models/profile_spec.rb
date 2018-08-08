9ce7b21b3289c66a77c91e7bde5107204d6d5478
Smithe941 <onupriy94@gmail.com>
2018-08-08 17:18:41 +0300

    Small fixes

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Profile, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:work_experiences).dependent(:destroy) }
   end
 
   describe 'validations' do
