6acb5af62a06be0fb8f6267bb8ea3d4985f65cbd
DeeMak13 <dima.m@active-bridge.com>
2018-08-10 15:13:51 +0300

    Move poltergist config to separate file

--- spec/support/poltergeist.rb
+++ spec/support/poltergeist.rb
@@ -1,9 +1,12 @@
+require 'capybara/rspec'
 require 'capybara/poltergeist'
 
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 Capybara.default_max_wait_time = 10
 Capybara.ignore_hidden_elements = false
+Capybara.server = :puma, {Silent: true}
+Capybara.default_max_wait_time = 20
 
 poltergeist_options = { js_errors: false,
                         timeout: 45,
